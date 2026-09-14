163d1c34ad182ae73767a887cdae683646e4bfd0
Flavien CANTINOTTI <flavien.cantinotti@gmail.com>
2026-09-14 11:59:05 +0000

    Harmonise les encarts de home.php avec le reste du site (bordure pleine, titre noir au lieu d'ambre)

--- fiche.php
+++ fiche.php
@@ -54,7 +54,7 @@ if ($id > 0) {
         <meta name="robots" content="noindex, follow">
         <title>Film introuvable · Cinévo</title>
     <?php endif; ?>
-    <link rel="stylesheet" type="text/css" href="css/style.css?v=28">
+    <link rel="stylesheet" type="text/css" href="css/style.css?v=29">
 </head>
 <body>
 
--- mes-avis.php
+++ mes-avis.php
@@ -44,7 +44,7 @@ if (baseDisponible()) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex, nofollow">
-    <link rel="stylesheet" type="text/css" href="css/style.css?v=28">
+    <link rel="stylesheet" type="text/css" href="css/style.css?v=29">
     <title>Cinévo · Mes avis</title>
 </head>
 <body>
